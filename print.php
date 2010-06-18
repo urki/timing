8a62c823303b978b007564d338bc6d7c7fe4889a
uros.gabrovec <uros.gabrovec@cc608b78-0b83-11df-9441-b1846cd9856d>
2010-06-18 06:02:39 +0000

    popravljeno

---
+++ print.php
@@ -0,0 +1,15 @@
+//<?php
+//$handle = printer_open(ML-1710);
+//printer_write($handle, "Text to print");
+//printer_close($handle);
+//?>
+ffdsafdafdasfdasfdsa
+fdsafdasfsda
+
+//<form>
+//<input type="button" value="Print This Page" onClick="window.print()" />
+//</form>
+<?php
+
+echo "ffadsfdsafa";
+echo "<script>window.print()</script>";
